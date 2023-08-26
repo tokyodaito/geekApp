d6ac31cec9019e3b49a7080ccf1247d7fd868513
Bogdan <bogdanbogotov@gmail.com>
2023-08-26 23:34:33 +0300

    delete file

--- app/src/main/java/com/bogsnebes/geekapp/ui/screens/main_screen/elm/StoreFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.bogsnebes.geekapp.ui.screens.main_screen.elm
-
-import vivid.money.elmslie.rx3.ElmStoreCompat
-
-fun storeFactory() = ElmStoreCompat(
-    initialState = State(),
-    reducer = Reducer(),
-    actor = Actor()
-).start()
